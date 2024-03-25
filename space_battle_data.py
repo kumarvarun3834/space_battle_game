316ca8047f8d530644dfcd39e599124a6c6c5e8f
varun3834 <139316140+varun3834@users.noreply.github.com>
2024-03-25 17:53:08 +0530

    Update space_battle_data.py

--- space_battle_data.py
+++ space_battle_data.py
@@ -1,4 +1,3 @@
-
 from all_colours_fonts import * 
 game_over = False
 end_screen_displayed = False
@@ -27,15 +26,15 @@ HEALTH_FONT = pygame.font.SysFont('comicsans', 40)
 WINNER_FONT=pygame.font.SysFont("comicsans",100)
 
 pygame.mixer.init()
-bullet_hit_sound=pygame.mixer.Sound("pygames/space_battle/Assets/Grenade+1.mp3")
-bullet_fire_sound=pygame.mixer.Sound('pygames/space_battle/Assets/Gun+Silencer.mp3')
-jet=pygame.mixer.Sound("pygames/space_battle/Assets/good-jetpack-sound-loop-96693.mp3")
-title="pygames/space_battle/Assets/the_field_of_dreams.mp3"
+bullet_hit_sound=pygame.mixer.Sound("Assets/Grenade+1.mp3")
+bullet_fire_sound=pygame.mixer.Sound('Assets/Gun+Silencer.mp3')
+jet=pygame.mixer.Sound("Assets/good-jetpack-sound-loop-96693.mp3")
+title="Assets/the_field_of_dreams.mp3"
 SPACE_SHIP_WIDTH,SPACE_SHIP_HEIGHT=60,60
-RED_SHIP_image=pygame.image.load("pygames/space_battle/Assets/white_ship.png")
+RED_SHIP_image=pygame.image.load("Assets/white_ship.png")
 RED_SHIP=pygame.transform.rotate(pygame.transform.scale(RED_SHIP_image,(SPACE_SHIP_WIDTH,SPACE_SHIP_HEIGHT)),90)
-YELLOW_SHIP_image=pygame.image.load("pygames/space_battle/Assets/blue_ship.png")
+YELLOW_SHIP_image=pygame.image.load("Assets/blue_ship.png")
 YELLOW_SHIP=pygame.transform.rotate(pygame.transform.scale(YELLOW_SHIP_image,(SPACE_SHIP_WIDTH,SPACE_SHIP_HEIGHT)),-90)
-space=pygame.transform.scale(pygame.image.load("pygames/space_battle/Assets/space.png"),(WIDTH,HEIGHT))
+space=pygame.transform.scale(pygame.image.load("Assets/space.png"),(WIDTH,HEIGHT))
 
-logo=pygame.transform.scale(pygame.image.load("pygames/space_battle/Assets/BattleSpaceHomeScreen.png"),(WIDTH,HEIGHT))
+logo=pygame.transform.scale(pygame.image.load("Assets/BattleSpaceHomeScreen.png"),(WIDTH,HEIGHT))
